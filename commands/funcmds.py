b6432e2b49fdb8c34482ef4685284286f76d9db2
jammy679 <julia.calpito@gmail.com>
2025-01-10 16:52:49 +0800

    fixes

---
+++ commands/funcmds.py
@@ -0,0 +1,233 @@
+import discord
+from discord.ext import commands
+import requests
+import random
+import asyncio
+import html
+import string
+import asyncpg
+
+class Fun(commands.Cog):
+    def __init__(self,bot):
+        self.bot = bot
+        self.scramble_in_use = False
+        self.trivia_in_use = False
+
+    @commands.command()
+    async def scramble(self,ctx):
+        if self.scramble_in_use:
+            await ctx.send('There is already scramble game running.')
+        else:
+            self.scramble_in_use = True
+            ans = ''    
+            while len(ans) <= 4:
+                with open('commands/google-10000-english.txt','r') as words_file:
+                    lines = words_file.readlines()
+                    ans = random.choice(lines)
+                    ans = ans.strip()
+
+            scrambled = ''
+            word = [letter for letter in ans]
+            for letter in ans:
+                choice = random.choice(word)
+                scrambled += choice 
+                word.remove(choice)
+            await ctx.send('Unscramble the following: ' + scrambled)
+
+            #answering algorithm
+            def check(guess):
+                return guess.author.bot == False 
+            finished = False
+            number_of_guesses = 0
+            points = 0
+            hint_text =''
+            if len(ans) > 12:
+                points = 10
+            elif len(ans) > 8 and len(ans) < 12:
+                points = 6 
+            elif len(ans) > 6 and len(ans) <= 8:
+                points = 4 
+            else:
+                points = 2 
+            while not finished and number_of_guesses != 8:
+                guess = ''
+                try:
+                    guess = await self.bot.wait_for('message', check = check, timeout = 10.0)
+                    if guess.content == ans:
+                        await ctx.send('Correct! <@!{}> got it right! <a:correct:839094567609434174> +{} points {}'.format(guess.author.id, str(points), hint_text))
+                        await self.update_points(guess.author.id, points, ctx.guild.id)
+                        finished = True
+                    else:
+                        number_of_guesses += 1
+                        if number_of_guesses == 4:
+                            dash = ' _'*(len(ans)-1)
+                            await ctx.send('💡Hint: `' + ans[:1] + dash + '`')
+                            hint_text = '(Hint used)'
+                            points = int(points / 2) 
+                except asyncio.TimeoutError:
+                    await ctx.send('Times up! The word was \'' + ans + '\'! <a:shakingbell:839095524187176970>')
+                    finished = True
+            if number_of_guesses == 8:
+                await ctx.send('You\'re out of guesses! The word was \''+ ans + '\'! <a:wrong:839094469173182484>')
+            self.scramble_in_use = False 
+
+    async def update_points(self, userid, points, guildid):
+        user_points = await self.bot.pg_con.fetch("SELECT points FROM \"{}_leaderboard\" WHERE user_id = \'{}\'".format(str(guildid), str(userid)))
+        user_points= [list(u)[0] for u in user_points][0]
+        user_points += points
+        await self.bot.pg_con.execute("UPDATE \"{}_leaderboard\" SET points = {} WHERE user_id = \'{}\'".format(str(guildid), str(user_points), str(userid)))
+
+    @commands.command()
+    async def trivia(self,ctx):
+        if self.trivia_in_use:
+            await ctx.send('There is already a trivia game running.')
+        else:
+            self.trivia_in_use = True
+            trivia_api = "https://opentdb.com/api.php?amount=1"
+            req = requests.get(url = trivia_api)
+            print(req.json())
+            question = req.json()['results'][0]
+            q = html.unescape(question['question'])
+
+
+            triviaembed = discord.Embed(
+                title = 'Trivia',
+                description = q,
+                colour = discord.Colour.orange()
+            )
+
+            answers = {}
+            correct = []
+            options = question['incorrect_answers']
+            options.append(question['correct_answer'])
+            ans_text = ''  
+            difficulties = {
+                'hard':'https://stoffe.kawaiifabric.com/images/product_images/large_img/solid-red-fabric-Robert-Kaufman-USA-Red-179485-1.JPG', 
+                'medium':'https://paperpackagingplace.com/wp-content/uploads/2016/01/Buttercup-Yellow.jpg', 
+                'easy':'https://www.deckleedge.co.za/wp-content/uploads/2017/12/9064ed2c814cf785ca71638dd2102b099281f0fa.jpg'
+            }
+
+            if question['type'] == 'multiple':
+                letters = [letter for letter in string.ascii_uppercase]
+                # randomises order of answers
+                for i in range(len(question['incorrect_answers'])):
+                    choice = random.choice(options)
+                    if choice == question['correct_answer']:
+                        choice = html.unescape(choice)
+                        correct = [(letters[i]).lower(), choice.lower()]
+                    answers[letters[i]] = choice
+                    options.remove(choice)
+                    ans_text += '**' + letters[i] + '.** ' + choice + '\n'
+
+            elif question['type'] == 'boolean':
+                ans_text = 'True or False'
+                correct = [(question['correct_answer']).lower()]
+
+            triviaembed.add_field(name='\u200b', value = ans_text, inline = False)
+            triviaembed.set_footer(text = question['category'])
+            triviaembed.set_author(name = question['difficulty'], icon_url = difficulties[question['difficulty']])
+
+            def user_ans(answer):
+                if question['type'] == 'multiple':
+                    k = ' '.join(answers.keys())
+                    v = ' '.join(answers.values())
+                    return answer.author.bot == False and (answer.content.lower() in k.lower() or answer.content.lower() in v.lower())
+                elif question['type'] == 'boolean':
+                    o = ' '.join(options)
+                    return answer.author.bot == False and answer.content.lower() in o.lower()
+            answered = False
+            guesses = 0
+            points = 0
+        
+            await ctx.send(embed = triviaembed)
+
+            #answering algorithm
+
+            if question['difficulty'] == 'hard':
+                points = 6
+            elif question['difficulty'] == 'medium':
+                points = 4
+            elif question['difficulty'] == 'easy':
+                points = 2
+            msg = ''
+            while not answered:
+                try:
+                    msg = await self.bot.wait_for('message', check = user_ans, timeout = 15.0)
+                    if msg.content.lower() in correct:
+                        await ctx.send('<a:correct:839094567609434174> Correct! <@!{}> got it right! 🎉+{} points'.format(str(msg.author.id), str(points))) 
+                        await self.update_points(msg.author.id, points, ctx.guild.id)
+                        answered = True
+                    else:
+                        guesses += 1
+                        points = points / 2
+                        if question['type'] == 'multiple':
+                            if guesses != 2:
+                                await ctx.send('<a:shakingbell:839095524187176970> Incorrect! You have one guess left.')
+                            else:
+                                await ctx.send('<a:wrong:839094469173182484> Incorrect! You have zero guesses left. The correct answer was \'' + html.unescape(question['correct_answer']) + '\'! 0 points earned.')
+                                answered = True
+                        elif question['type'] == 'boolean':
+                            await ctx.send('<a:wrong:839094469173182484> Incorrect! The correct answer was \'' + question['correct_answer']+ '\'! 0 points earned.')
+                            answered = True
+                except asyncio.TimeoutError:
+                    await ctx.send('<a:shakingbell:839095524187176970> Times up! The correct answer was \''+ html.unescape(question['correct_answer']) + '\'! 0 points earned.')
+                    answered = True
+            self.trivia_in_use = False
+
+    @commands.command()
+    async def leaderboard(self,ctx):
+        users_points = await self.bot.pg_con.fetch("SELECT * FROM \"{}_leaderboard\"".format(str(ctx.guild.id)))
+        users_points = [tuple(u) for u in users_points]
+        def second_elem(elem):
+            return elem[1]
+        users_points.sort(key=second_elem, reverse=True)
+        users= [self.bot.get_user(int(u[0])) for u in users_points]
+
+        user_text= ''
+        points_text = ''
+        place = 1
+        for i in range(len(users_points)):
+            points_text += str(users_points[i][1]) + '\n'
+            try:
+                if place == 1:
+                    user_text += "🥇 " + users[i].name + '\n'
+                elif place == 2:
+                    user_text += "🥈 " + users[i].name + '\n'
+                elif place == 3:
+                    user_text += "🥉 " + users[i].name + '\n'
+                else:
+                    user_text += "`" + str(place) + "` " + users[i].name + '\n'
+                place +=1
+            except AttributeError:
+                continue
+
+
+        board = discord.Embed(
+            title = 'Leaderboard!!',
+            description = 'Earn points by playing my minigames!',
+            colour = discord.Colour.dark_red()
+        )
+        
+        #user_text = "🥇 Example User\n🥈 Example User #2\n🥉 Example User #3\n`4` Example User #4\n`5` Example User #5"
+        #points_text = "112\n40\n36\n20\n17"
+
+        board.add_field(name='User', value=user_text, inline=True)
+        board.add_field(name='Points', value=points_text, inline=True)
+        await ctx.send(embed = board)
+
+    @commands.command()
+    async def fact(self,ctx):
+        url = 'https://uselessfacts.jsph.pl/random.json?language=en'
+        req = requests.get(url=url)
+        fact_resp = req.json()
+        factembed = discord.Embed(
+            title = 'Incoming fact!',
+            colour  = discord.Colour.from_rgb(252, 211, 154)
+        )
+        factembed.set_author(name = 'Get another fact by typing \'!j fact\'!', icon_url = 'https://www.freeiconspng.com/thumbs/lightbulb-png/lightbulb-png-no-circle--lightbulb-10.png')
+        factembed.add_field(name= '\u200b', value = fact_resp['text'], inline=False)
+        factembed.set_footer(text = 'Fact from ' + fact_resp['source'] + ' - ' + fact_resp['source_url'])
+        await ctx.send(embed = factembed)
+
+async def setup(bot):
+    await bot.add_cog(Fun(bot))
---
+++ commands/poll.py
@@ -0,0 +1,220 @@
+import discord
+from discord.ext import commands
+
+class Poll:
+    def __init__(self,name,owner):
+        self.name = name
+        self.items = {}
+        self.owner = owner
+    
+    def additem(self, key):
+        #format - pollitem: [], where list has list of users that voted for that item
+        self.items[key] = [] 
+    
+    def removeitem(self,key):
+        self.items.pop(key)
+    
+    def addvote(self,key,user):
+        if user not in self.items[key]:
+            # checks if they already voted
+            self.removevote(user)
+            self.items[key].append(user)
+        else:
+            return 'Same item'
+    
+    def removevote(self, user):
+        for k,v in (self.items).items():
+            if user in v:
+                v.remove(user)
+                break
+
+class PollCog(commands.Cog):
+    def __init__(self,bot):
+        self.bot = bot
+        self.poll_running = False
+        self.current_poll = None
+        self.pollembed = None
+    
+    @commands.command()
+    async def startpoll(self,ctx,*, name):
+        if self.poll_running == True:
+            await ctx.send("There is a poll already running.")
+        else:
+            self.poll_running = True
+            def check_if_author(msg):
+                return msg.author == ctx.author
+            self.current_poll = Poll(name, ctx.author.id)
+            await ctx.send("<@!" + str(ctx.author.id) + "> has started a poll named \'" + name + "\'!")
+    
+        
+    @commands.command(aliases = ['add'])
+    async def polladd(self, ctx, *, item):
+        if self.poll_running == False:
+            await ctx.send('There is currently no poll running.')
+        else:
+            if ctx.author.id == self.current_poll.owner:
+                in_poll = False
+                for key, value in (self.current_poll.items).items():
+                    if key == item:
+                        await ctx.send('That item is already in the poll.')
+                        in_poll = True
+                        break
+                if not in_poll:
+                    self.current_poll.additem(item)
+                    await ctx.send('Added!')
+                    if self.pollembed != None:
+                        await self.update(ctx)        
+            else:
+                await ctx.send('Only the owner, <@!' + str(self.current_poll.owner) + '>, can add or remove items from the current poll.')
+    
+    @commands.command(aliases = ['remove'])
+    async def pollremove(self,ctx,*,item):
+        if self.poll_running == False:
+            await ctx.send('There is currently no poll running.')
+        else:
+            if ctx.author.id == self.current_poll.owner:
+                in_poll = False
+                for key,value in (self.current_poll.items).items():
+                    if key == item:
+                        self.current_poll.removeitem(item)
+                        in_poll = True 
+                        await ctx.send('Removed!')
+                        if self.pollembed != None:
+                            await self.update(ctx)        
+                        break
+
+                if not in_poll:
+                    await ctx.send('That item is not in the poll.')
+            else:
+                await ctx.send('Only the owner, <@!' + str(self.current_poll.owner) +'>, can add or remove items from the current poll.')
+    
+    #command supposed to not be available to users
+    async def update(self,ctx):
+        if not self.poll_running:
+            print('not running')
+        else:
+            item_str = ''
+            value_str = ''
+            for key,value in (self.current_poll.items).items():
+                item_str += key + '\n'
+                value_str += str(len(value)) + '\n' 
+            self.pollembed.set_field_at(0, name = 'Item', value = item_str, inline = True)
+            self.pollembed.set_field_at(1, name = 'Votes', value = value_str, inline = True)
+            await self.pollmsg.edit(embed = self.pollembed)
+    
+    @commands.command()
+    async def vote(self,ctx,*,item):
+        if self.poll_running == False:
+            await ctx.send('There is currently no poll running.')
+        else:
+            in_poll = False
+            for key,value in (self.current_poll.items).items():
+                if item == key:
+                    in_poll = True
+                    break
+            if in_poll:
+                voted = self.current_poll.addvote(item, ctx.author.name)
+                if voted != None:
+                    await ctx.send('You have already voted for this item.')
+                else:
+                    if self.pollembed != None:
+                        await self.update(ctx)        
+                    await ctx.send('Voted!')
+            else:
+                await ctx.send('That item is not in the poll.')
+    
+    @commands.command()
+    async def unvote(self,ctx):
+        if not self.poll_running:
+            await ctx.send("There is currently no poll running.")
+        else:
+            self.current_poll.removevote(ctx.author.name)
+            await self.update(ctx)        
+
+
+    @commands.command()
+    async def poll(self,ctx):
+        if self.poll_running == False:
+            await ctx.send('There is currently no poll running.')
+        else:
+            item_str = ''
+            value_str = ''
+            for key,value in (self.current_poll.items).items():
+                item_str += key + '\n'
+                value_str += str(len(value)) + '\n' 
+            
+            if item_str == '':
+                await ctx.send('There are currently no items in the poll.')
+            else:
+                self.pollembed = discord.Embed(
+                    title = self.current_poll.name,
+                    colour = discord.Colour.red()
+                )
+
+                self.pollembed.add_field(name = 'Item', value = item_str, inline= True)
+                self.pollembed.add_field(name = 'Votes', value = value_str, inline = True)
+                self.pollmsg = await ctx.send(embed = self.pollembed)
+    
+    @commands.command(aliases = ['end'])
+    async def endpoll(self,ctx):
+        if not self.poll_running:
+            await ctx.send('There is currently no poll running.')
+        else:
+            if ctx.author.id == self.current_poll.owner:
+                keys = [k for k,v in (self.current_poll.items).items()]
+                values = [len(v) for k,v in (self.current_poll.items).items()]
+                if not keys or len(keys) == 1:
+                    await ctx.send('Poll cancelled.')
+                    self.poll_running = False
+                else:
+                    highest_item = keys[0] 
+                    highest_value = values[0] 
+                    tied = [0] 
+                    for i in range(1,len(values)):
+                        if values[i] > highest_value:
+                            highest_value = values[i] 
+                            highest_item = keys[i]
+                            tied = [i]
+                        elif values[i] == highest_value:
+                            tied.append(i)
+
+                    await ctx.send('<a:shakingbell:839095524187176970> Here are the final results! <a:shakingbell:839095524187176970>', embed = self.pollembed)
+                    if len(tied) > 1:
+                        for i in range(1,len(tied)-1):
+                            highest_item += ', ' + keys[tied[i]]
+                        highest_item += ' and ' + keys[tied[len(tied)-1]]
+                        await ctx.send('<a:sparklecolors:839827975418675211> `' + highest_item + '` tied with `' + str(highest_value) + '` votes! <a:sparklecolors:839827975418675211>')
+                    else: 
+                        await ctx.send('<a:sparklecolors:839827975418675211> `' + highest_item + '` won the poll with `' + str(highest_value) + '` votes! <a:sparklecolors:839827975418675211>')
+                    self.poll_running = False
+            else:
+                await ctx.send('Only the owner, <@!' + str(self.current_poll.owner) +'>, can end the current poll.')
+    
+    @commands.command()
+    async def pollhelp(self,ctx):
+        phelp = discord.Embed(
+            title = 'Poll commands',
+            colour = discord.Colour.blurple(),
+            description = 'Format: `!j <command name>`'
+        )
+        commands = [
+            '`startpoll <name of poll>` - start a poll\n',
+            '`polladd (or add) <item>` - add an item to the poll **(only the owner of the poll can do this)**\n',
+            '`pollremove (or remove) <item>` - remove an item from the poll **(only the owner of the poll can do this)**\n',
+            '`endpoll (or end)` - end/cancel the poll **(only the owner of the poll can do this)**\n',
+            '`vote <item in poll>` - vote for an item in the poll\n',
+            '`unvote` - remove your vote from the poll\n',
+            '`poll` - view the poll\n'
+        ]
+        cmd_str =''
+        for c in commands:
+            cmd_str += c
+        phelp.add_field(name = 'Commands', value = cmd_str, inline = False)
+        await ctx.send(embed = phelp)
+
+
+        
+async def setup(bot):
+    await bot.add_cog(PollCog(bot))
+
+        
